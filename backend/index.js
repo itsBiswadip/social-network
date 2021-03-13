1c6b6a8aeee4f62d197d6d6d5f5016cbe556079f
Biswadip Bhowmick <tobiswadip@gmail.com>
2021-03-13 18:49:20 +0530

    Initial Setup

---
+++ backend/index.js
@@ -0,0 +1,50 @@
+// Instantiate app
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const { errors } = require('celebrate');
+const path = require('path');
+
+const app = express();
+
+//handle cors
+app.use(cors());
+
+//handle post or url encoded data
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+//request logger
+app.use(morgan('dev'));
+
+
+/**
+ * Error Handlers
+ */
+app.use(errors()); // api validation errors
+
+app.use((error,req,res,next) => {
+    res.status(error.status || 500).json({
+        "error": {
+            message: error.message || 'Something is wrong'
+        }
+    });
+});
+
+// serve static files of React app
+app.use(express.static(path.resolve(__dirname, '../frontend/build')));
+
+app.use("/api", (req, res) => {
+    res.json({ message: "Hello!" });
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+});
+
+//Configure the Server
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, ()=> {
+    console.log(`Listening on Port: ${PORT}`);
+});
