539788949d5d31e67128dc080ca078b50fdc1882
Biswadip Bhowmick <tobiswadip@gmail.com>
2021-03-14 12:38:17 +0530

    Add model for user db

---
+++ backend/src/domain/User/userModel.js
@@ -0,0 +1,42 @@
+const db = require('../../common/db');
+const { QueryTypes } = require('sequelize');
+const UserSchema = require('./userSchema');
+
+const userModel = {};
+
+userModel.createUser = async(email, password, userName = '') => {
+    let query = `INSERT 
+            INTO User (email, password, userName) 
+            VALUES (?, ?, ?)`;
+    let [newUser] = await db.query(query, {
+        raw: true,
+        type: QueryTypes.INSERT,
+        replacements: [email, password, userName = '']
+    })
+
+    return newUser;
+}
+
+userModel.findUserByUserId = async(userId, attributes=['*']) => {
+    let query = `SELECT ${attributes.join()} FROM User WHERE userID = ?`;
+    let [user] = await db.query(query, {
+        raw: true,
+        type: QueryTypes.SELECT,
+        replacements: [userId]
+    })
+
+    return user;
+}
+
+userModel.findUserByEmail = async(email, attributes=['*']) => {
+    let query = `SELECT ${attributes.join()} FROM User WHERE email = ?`;
+    let [user] = await db.query(query, {
+        raw: true,
+        type: QueryTypes.SELECT,
+        replacements: [email]
+    })
+
+    return user;
+}
+
+module.exports = userModel;
