21605a97cb88f6beff081afa3e94a4a8b6e61cc6
Biswadip Bhowmick <tobiswadip@gmail.com>
2021-03-15 19:39:11 +0530

    Add app homepage for autologin

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,50 @@
+import React, { useContext, useEffect } from 'react';
+import { useHistory } from 'react-router';
+import { UserContext } from '../contexts/UserProvider';
+import { message } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
+
+const LandingPage = () => {
+    const userInfo = useContext(UserContext);
+    const history = useHistory();
+
+    useEffect(()=>{
+        const isLoggedIn = !!(userInfo.user);
+        const authToken = localStorage.getItem('authToken');
+        if(!isLoggedIn) {
+            if(authToken) {
+                fetch(`/api/users`,{
+                    headers: { 
+                        'Content-Type': 'application/json',
+                        'Authorization' : authToken
+                    }
+                })
+                .then(async (res) => {
+                    const data = await res.json();
+                    if(!res.ok) {
+                        data.statusCode = data.statusCode || res.status;
+                        return Promise.reject(data);
+                    }
+                    userInfo.setUser(data.user);
+			        history.push('/home');
+                })
+                .catch(error => {
+                    console.log('auto login error',error)
+                    let errorMessage = (error.statusCode && error.message)? error.message : 'Something is wrong';
+                    message.error(errorMessage);
+                });
+            }
+            else
+                history.push('/login')
+        }
+
+    },[]);
+
+    return (
+        <div className="App-header">
+            <LoadingOutlined />
+        </div>
+    );
+}
+
+export default LandingPage;
