6f9f12c204f75114721bacbc52e8c4e0e3417398
Biswadip Bhowmick <tobiswadip@gmail.com>
2021-03-15 19:28:17 +0530

    Enhance Router component for protected sections

---
+++ frontend/src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+import { Route, Redirect } from 'react-router-dom'
+import { UserContext } from '../../contexts/UserProvider';
+
+const ProtectedRoute = ({children, ...rest}) => {
+    const userInfo = useContext(UserContext);
+    const isLoggedIn = !!(userInfo.user);
+        
+    return (
+        <Route 
+            {...rest} 
+            render={() => {
+                return isLoggedIn? children : <Redirect to='/login' />
+            }}
+        />
+    );
+}
+
+export default ProtectedRoute;
