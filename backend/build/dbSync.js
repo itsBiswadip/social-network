41a1ffbc245fa747dd8e1f5fcfe609e21bd3fd29
Biswadip Bhowmick <tobiswadip@gmail.com>
2021-03-16 11:26:08 +0530

    Add post db sync to initial setup script

--- backend/build/dbSync.js
+++ backend/build/dbSync.js
@@ -1,10 +1,12 @@
 const db = require("../src/common/db");
 const UserSchema = require("../src/domain/User/userSchema");
+const PostSchema = require("../src/domain/Post/postSchema");
 
 
 db.authenticate()
 .then(async() => {
     await UserSchema.sync();
+    await PostSchema.sync();
     console.log("Database Synced Successfully");
 })
 .catch(error => console.log(error));
